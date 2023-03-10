9db16bec909d01337b4a113f03b9401ef98f6c20
ouaelesi <ouael2019esi@gmail.com>
2023-03-10 23:41:03 +0100

    complete: NavBar + header

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,7 @@ import Image from "next/image";
 import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
 import NavBar from "@/components/shared/NavMenu";
+import Header from "@/components/homePage/Header";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,9 +16,9 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="" >
-        <NavBar/>
- 
+      <main className="">
+        <NavBar />
+        <Header/>
       </main>
     </>
   );
