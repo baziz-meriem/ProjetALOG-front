0b89005686d59a7863ce9b4814534ee4405c8fa8
ouaelesi <ouael2019esi@gmail.com>
2023-03-10 23:20:02 +0100

    add: the navBar

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,12 +4,18 @@ module.exports = {
     "./app/**/*.{js,ts,jsx,tsx}",
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
- 
+
     // Or if using `src` directory:
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "light-green": "#84C2AD",
+        "dark-green": "#5F9B85",
+        "creem-green": "#F2F8F2",
+      },
+    },
   },
   plugins: [],
-}
+};
