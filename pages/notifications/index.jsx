0793ecc325de28891ea5abaa36e2908e52125aaa
baziz_meriem <mrmbaziz@gmail.com>
2023-03-14 16:42:59 +0100

    Add array instead of static render

--- pages/notifications/index.jsx
+++ pages/notifications/index.jsx
@@ -5,12 +5,28 @@ import Notification from "@/components/notificationPage/Notification";
 import React from "react";
 
 const Notifications = () => {
+  const notifications = [
+    {
+      type: "Tentative de vol",
+      description: "Une tentative de vol a été enregistré au niveau du distributeur :115",
+    },
+    {
+      type: "Tentative de vol",
+      description: "Une tentative de vol a été enregistré au niveau du distributeur :115",
+    },
+    {
+      type: "Tentative de vol",
+      description: "Une tentative de vol a été enregistré au niveau du distributeur :115",
+    },
+  ];
   return (
     <div>
         <PageHeader title="Mes Notifications" description="Rester au courant de toute nouvelle"/>
-        <Notification type="Tentative de vol" description="Une tentative de vol a été enregistré au niveau du distributeur :115"/>
-        <Notification type="Tentative de vol" description="Une tentative de vol a été enregistré au niveau du distributeur :115"/>
-        <Notification type="Tentative de vol" description="Une tentative de vol a été enregistré au niveau du distributeur :115"/>
+        {
+          notifications.map((notification,index)=>(
+            <Notification key={index} type={notification.type} description={notification.description}/>
+          ))
+        }
 
         <div className="flex justify-end">
               <button class="btn-green px-11 py-2.5 mt-4 light-grey">
