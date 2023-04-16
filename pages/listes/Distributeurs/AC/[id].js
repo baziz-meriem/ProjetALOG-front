c86937cbccf494d1af14ba14d8218f42c13b0fd5
meryeemdifangel <jm_dif@esi.dz>
2023-04-16 15:39:37 +0100

    integration of drinks

--- pages/listes/Distributeurs/AC/[id].js
+++ pages/listes/Distributeurs/AC/[id].js
@@ -1,28 +1,21 @@
 import Image from "next/image";
-import React ,{useState,useRef,useEffect} from "react";
+import React, { useState, useRef, useEffect } from "react";
 import PageHeader from "@/components/shared/PageHeader";
 import AffectationCard from "@/components/details/affectationCard";
 import InfoCard from "@/components/details/infoCard";
 import BoissonCard from "@/components/details/boissonCard";
 import MapOverlay from "@/components/dashboard/MapOverlay";
 import dynamic from 'next/dynamic';
+import { useRouter } from "next/router";
+import { getBoissons } from "@/services/boisson";
 
 const Map = dynamic(() => import('@/components/dashboard/Map'), { ssr: false });
 
 
 const DistributeursAC = () => {
-  const Data = [
-    { id: 1, label: "Latte Coffee", prix:"70",available:"true",image:"/icons/coffee.svg" },
-    { id: 2, label: "Latte Coffee",prix:"70",available:"true",image:"/icons/coffee.svg"},
-    { id: 3, label: "Latte Coffee" ,prix:"70",available:"false",image:"/icons/coffee.svg" },
-    { id: 4, label: "Latte Coffee",prix:"70",available:"false",image:"/icons/coffee.svg" },
-    { id: 5, label: "Latte Coffee",prix:"70",available:"true" ,image:"/icons/coffee.svg"},
-    { id: 6, label: "Latte Coffee" ,prix:"70",available:"false",image:"/icons/coffee.svg"},
-    { id: 7, label: "Latte Coffee",prix:"70",Role:"AC" ,Region:"true",image:"/icons/coffee.svg"},
-    { id: 8, label: "Latte Coffee" ,prix:"70",available:"false",image:"/icons/coffee.svg"},
-  ];
+  const [data, setData] = useState([])
   const [showDetails, setshowDetails] = useState(false);
-
+  const router = useRouter();
   useEffect(() => {
     const handleKeyDown = (event) => {
       if (event.key === "m" && !showDetails) {
@@ -31,26 +24,36 @@ const DistributeursAC = () => {
         setshowDetails(false);
       }
     };
-
     document.addEventListener("keydown", handleKeyDown);
     return () => {
       document.removeEventListener("keydown", handleKeyDown);
     };
   }, [showDetails]);
+  //get all drinks
+  useEffect(() => {
+    const { id } = router.query;
+    const getAllBoissons = async () => {
+    const { data } = await getBoissons(id)
+    setData(data.data)
+    }
+     getAllBoissons()
+  }, [])
+
+
 
   return (
     <div>
-         
+
       <PageHeader
         title="Distributeur Details"
         description="Affiche les informations détaillées du distributeur"
       />
       <InfoCard title="Distributeur Infos" />
       <div className="flex gap-4 h-40">
-      <AffectationCard title="AM du distributeur" />
-      <div className=" w-1/2  mt-6 shadow-all rounded-lg bg-transparent overflow-hidden">
-      {showDetails? (<MapOverlay/>):(<Map />) }
-      </div>
+        <AffectationCard title="AM du distributeur" />
+        <div className=" w-1/2  mt-6 shadow-all rounded-lg bg-transparent overflow-hidden">
+          {showDetails ? (<MapOverlay />) : (<Map />)}
+        </div>
       </div>
       <div className="p-10 mt-6 shadow-all rounded-lg bg-transparent">
         <div className="flex items-center">
@@ -67,11 +70,11 @@ const DistributeursAC = () => {
         </div>
 
         <div className="grid grid-cols-3 gap-16">
-        {Data.map((rowData) => (
-           <BoissonCard key={rowData.id} data={rowData} numColumns="6" toAdd="ADMs"/>
+          {data.length>0 && data.map((rowData) => (
+            <BoissonCard key={rowData.id} data={rowData} numColumns="6" toAdd="ADMs" />
           ))}
         </div>
-        </div>
+      </div>
     </div>
   );
 };
